049ef0f1c7556b98d71884bdb0754ce169b1fb33
linux-jedi <cbthomas1997>
2020-03-19 16:52:25 -0500

    Added basic trace optimizations

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,8 @@
 use std::convert::TryInto;
+use std::collections::HashMap;
 use std::io::{self, Write, Read};
 
-use crate::parser::Instruction;
-use crate::parser::Program;
+use crate::parser::{Instruction, Op, Program};
 
 struct InterpreterState {
     memory: Vec<u8>,
@@ -20,21 +20,36 @@ impl InterpreterState {
     }
 }
 
+pub fn serialize_ops(ops: &Vec<Op>) -> String {
+    let mut string = String::with_capacity(ops.len() * 2);
+    for op in ops.iter() {
+        string.push_str(&op.to_string());
+    }
+
+    string
+}
+
 // TODO: have this return Result(OK, err)
 pub fn interpret_program(program: &Program) {
     let mut state = InterpreterState::new(30000);
+    
+    // Setup tracing infra
+    let mut curr_trace: Vec<Op> = Vec::new();
+    let mut trace_count: HashMap<String, usize> = HashMap::new();
 
     while state.pc < program.ops.len() {
         // execute instruction
         let op = &program.ops[state.pc];
 
         match op.op_type {
-            Instruction::IncPtr => state.ptr += op.arg,
-            Instruction::DecPtr => state.ptr -= op.arg,
+            Instruction::IncPtr => state.ptr += op.arg as usize,
+            Instruction::DecPtr => state.ptr -= op.arg as usize,
             Instruction::IncData => 
-                state.memory[state.ptr] = state.memory[state.ptr].wrapping_add(op.arg.try_into().unwrap()),
+                state.memory[state.ptr] = 
+                    state.memory[state.ptr].wrapping_add(op.arg.try_into().unwrap()),
             Instruction::DecData =>
-                state.memory[state.ptr] = state.memory[state.ptr].wrapping_sub(op.arg.try_into().unwrap()),
+                state.memory[state.ptr] = 
+                    state.memory[state.ptr].wrapping_sub(op.arg.try_into().unwrap()),
             Instruction::Write => {
                 io::stdout().write_all(&[state.memory[state.ptr]]).unwrap();
             },
@@ -43,16 +58,31 @@ pub fn interpret_program(program: &Program) {
             },
             Instruction::JumpNotZero => {
                 if state.memory[state.ptr] != 0 {
-                    state.pc = op.arg;
+                    state.pc = op.arg.try_into().unwrap();
                 } else {
                     state.pc += 1;
                 }
             },
             Instruction::JumpZero => {
                 if state.memory[state.ptr] == 0 {
-                    state.pc = op.arg;
+                    state.pc = op.arg.try_into().unwrap();
                 } else {
                     state.pc += 1;
+                }
+            },
+            Instruction::LoopSetZero => state.memory[state.ptr] = 0,
+            Instruction::LoopMovePtr => {
+                while state.memory[state.ptr] != 0 {
+                    state.ptr = (state.ptr as i64 + op.arg).try_into().unwrap();
+                }
+            },
+            Instruction::LoopMoveData => {
+                if state.memory[state.ptr] != 0 {
+                    let move_to_ptr: usize = 
+                        (state.ptr as i64 + op.arg).try_into().unwrap();
+                    state.memory[move_to_ptr] = 
+                        state.memory[move_to_ptr].wrapping_add(state.memory[state.ptr]);
+                    state.memory[state.ptr] = 0;
                 }
             },
             Instruction::Invalid => (),
@@ -64,5 +94,24 @@ pub fn interpret_program(program: &Program) {
             Instruction::JumpNotZero => (),
             _ => state.pc += 1,
         }
+
+        // Trace
+        match op.op_type {
+            Instruction::JumpZero => curr_trace.clear(),
+            Instruction::JumpNotZero if curr_trace.len() > 0 => {
+                *trace_count.entry(serialize_ops(&curr_trace)).or_insert(0) += 1;
+                curr_trace.clear();
+            },
+            Instruction::JumpNotZero => (),
+            _ => curr_trace.push(*op),
+        }
+    }
+
+    // print trace
+    let mut trace_count_vec: Vec<_> = trace_count.iter().collect();
+    trace_count_vec.sort_by(|a, b| b.1.cmp(a.1));
+
+    for trace in trace_count_vec.iter() {
+        println!("{:10}\t{}", trace.0, trace.1);
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 use std::collections::LinkedList;
+use std::convert::TryInto;
 use std::io::BufRead;
 
 pub struct Program {
@@ -18,19 +19,45 @@ pub enum Instruction {
     Write,
     JumpZero,
     JumpNotZero,
+    LoopSetZero,
+    LoopMovePtr,
+    LoopMoveData,
 }
 
+#[derive(Clone, Copy)]
 pub struct Op {
     pub op_type: Instruction,
-    pub arg: usize,
+    pub arg: i64,
 }
 
 impl Op {
-    pub fn new(op_type: Instruction, arg: usize) -> Op {
+    pub fn new(op_type: Instruction, arg: i64) -> Op {
         Op {
             op_type,
             arg
         }
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut string = String::with_capacity(2);
+        
+        match self.op_type {
+            Instruction::IncPtr => string.push('>'),
+            Instruction::DecPtr => string.push('<'),
+            Instruction::IncData => string.push('+'),
+            Instruction::DecData => string.push('-'),
+            Instruction::JumpZero => string.push('['),
+            Instruction::JumpNotZero => string.push(']'),
+            Instruction::Read => string.push(','),
+            Instruction::Write => string.push('.'),
+            Instruction::LoopSetZero => string.push('s'),
+            Instruction::LoopMovePtr => string.push('p'),
+            Instruction::LoopMoveData => string.push('d'),
+            _ => string.push('x'),
+        }
+
+        string.push_str(&self.arg.to_string());
+        string
     }
 }
 
@@ -60,6 +87,60 @@ pub fn parse <R: BufRead>(r: &mut R) -> Result<Vec<Instruction>, io::Error> {
     Ok(instructions)
 }
 
+pub fn optimize_loop(ops: &Vec<Op>, start: usize) -> Vec<Op> {
+    let mut new_ops: Vec<Op> = Vec::new();
+
+    match ops.len() - start {
+        // Get repeated steps
+        // ex. [>], [<], [+], etc.
+        2 => {
+            let repeated_op = ops[start + 1];
+            match repeated_op.op_type {
+                Instruction::IncData | Instruction::DecData => 
+                    new_ops.push(Op::new(Instruction::LoopSetZero, 0)),
+                Instruction::IncPtr => 
+                    new_ops.push(Op::new(Instruction::LoopMovePtr, repeated_op.arg)),
+                Instruction::DecPtr =>
+                    new_ops.push(Op::new(Instruction::LoopMovePtr, -repeated_op.arg)),
+                _ => (),
+            }
+        },
+        // detect -<+> and ->+<
+        5 => {
+            let data_op_one = ops[start + 1];
+            let data_op_two = ops[start + 3];
+
+            if  matches!(data_op_one.op_type, Instruction::DecData) &&
+                matches!(data_op_two.op_type, Instruction::IncData) &&
+                data_op_one.arg == 1 &&
+                data_op_two.arg == 1 {
+                
+                let move_op_one = ops[start + 2];
+                let move_op_two = ops[start + 4];
+                if  matches!(move_op_one.op_type, Instruction::IncPtr) &&
+                    matches!(move_op_two.op_type, Instruction::DecPtr) &&
+                    move_op_one.arg == move_op_two.arg {
+                    
+                    new_ops.push(
+                        Op::new(Instruction::LoopMoveData, move_op_one.arg)
+                    );
+                }
+                else if matches!(move_op_one.op_type, Instruction::DecPtr) &&
+                        matches!(move_op_two.op_type, Instruction::IncPtr) &&
+                        move_op_one.arg == move_op_two.arg {
+
+                    new_ops.push(
+                        Op::new(Instruction::LoopMoveData, -move_op_one.arg)
+                    )
+                }
+            }
+        },
+        _ => (),
+    }
+    new_ops
+}
+
+// Merges repeated ops and performs loop optimizations
 pub fn merge_ops(instructions: &Vec<Instruction>) -> Program {
     let mut pc: usize = 0;
     let mut ops: Vec<Op> = Vec::new();
@@ -87,10 +168,19 @@ pub fn merge_ops(instructions: &Vec<Instruction>) -> Program {
 
                 // create jump from open to close bracket
                 let bracket_offset = open_bracket_stack.pop_back().unwrap();
-                ops[bracket_offset].arg = ops.len();
+                
+                // Try to optimizing the loop. If successful, replace the loop with
+                // the returned vector of operations.
+                let mut optimized_loop = optimize_loop(&ops, bracket_offset);
 
-                // create op for close bracket
-                ops.push(Op::new(Instruction::JumpNotZero, bracket_offset));
+                if optimized_loop.is_empty() {
+                    ops[bracket_offset].arg = ops.len().try_into().unwrap();
+                    ops.push(Op::new(Instruction::JumpNotZero, bracket_offset.try_into().unwrap()));
+                } else {
+                    ops.drain(bracket_offset..);
+                    ops.append(&mut optimized_loop);
+                }
+                
                 pc += 1;
             },
             _ => {
@@ -101,7 +191,7 @@ pub fn merge_ops(instructions: &Vec<Instruction>) -> Program {
                 }
 
                 // create new op using offset
-                let num_repeats: usize = pc - start;
+                let num_repeats: i64 = (pc - start).try_into().unwrap();
                 ops.push(Op::new(instruction.clone(), num_repeats));
             },
         }
